e6187489b8a9ca3845c4d24b5c73f8b7c6e38b6a
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2024-06-10 18:37:25 -0300

    s

--- checkout/src/App.tsx
+++ checkout/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [name, setName] = useState("");
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    
     setGreetMsg(await invoke("greet", { name }));
   }
 
@@ -17,35 +17,16 @@ function App() {
       <img className="market" src="../public/app-icon.png" />
 
       <div className="row">
-        <a href="https://vitejs.dev">
+        <a>
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app">
+        <a>
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org">
+        <a>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
