537cbbac1905c814b1a670cd49979956ed13cd19
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2024-06-11 15:38:46 -0300

    df

--- checkout/src/App.tsx
+++ checkout/src/App.tsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <div className="container">
       <img className="market" src="../public/app-icon.png" />
 
       <div className="row">
-        <a href="./html/contas.html">
+        <a href="../src/html/contas.html">
           <img className="pessoas" src="../public/33308.png" alt="pessoas" />
           <h1>Pessoas</h1>
         </a>
